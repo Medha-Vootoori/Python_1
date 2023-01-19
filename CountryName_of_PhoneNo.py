61f35c63edf88bb7d5e37a8fc5dff6e7cca401a8
Medha-Vootoori <121211005+Medha-Vootoori@users.noreply.github.com>
2023-01-19 14:02:25 +0530

    Add files via upload

---
+++ CountryName_of_PhoneNo.py
@@ -0,0 +1,38 @@
+import phonenumbers
+from phonenumbers import geocoder
+print("Number of phone numbers") 
+length_of_list=int(input())
+list_of_numbers=[]
+if length_of_list:
+    print("Enter your number in the following format")
+    print("countrycode xxxxxxxxxx")
+
+    for i in range(1,length_of_list+1):
+        print("Enter the phone number "+str(i))
+        number=input()
+        list_of_numbers.append(number)
+    for number in list_of_numbers:
+        ch_number=phonenumbers.parse(number, "CH")
+        print(number,end=" : ")
+        country=geocoder.description_for_number(ch_number, "en")
+        if len(country):
+            print(country)
+        else:
+            print("INVALID INPUT")
+else:
+    print("NO INPUT")
+
+
+
+
+
+
+
+
+
+"""from phonenumbers import carrier
+service_number=phonenumbers.parse(number, "RO")
+print("Service Name: ",end="")
+print(carrier.name_for_number(service_number, "en"))
+
+"""
---
+++ text_to_speech.py
@@ -0,0 +1,27 @@
+# import the pyttsx module inside program
+import pyttsx3
+w=input()
+# initializing the module
+engine = pyttsx3.init()
+
+# .say() function is used to speak the text you have written 
+# inside the function
+engine.say(w)
+
+# this is used to process and run the program commands
+engine.runAndWait()
+
+
+
+
+
+
+
+
+
+
+
+
+#Hey Vani! How are you? Hope you are fine and happy with your family. Your daughter Medha is very intelligent and brave. So be proud of your daughter.
+#Hello Sai Teja Chaganty. you need to be very thankful to your friend Medha. She really cares about you.
+#Hey Vaikuntam! Your grand daughter Medha is very intelligent and brave.She is the best.Megha is also good but Medha is the best
