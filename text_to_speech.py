8c7dcd1710cda4129107957c6e609f0d01227bcd
Medha-Vootoori <121211005+Medha-Vootoori@users.noreply.github.com>
2023-01-19 21:08:47 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -10,18 +10,3 @@ engine.say(w)
 
 # this is used to process and run the program commands
 engine.runAndWait()
-
-
-
-
-
-
-
-
-
-
-
-
-#Hey Vani! How are you? Hope you are fine and happy with your family. Your daughter Medha is very intelligent and brave. So be proud of your daughter.
-#Hello Sai Teja Chaganty. you need to be very thankful to your friend Medha. She really cares about you.
-#Hey Vaikuntam! Your grand daughter Medha is very intelligent and brave.She is the best.Megha is also good but Medha is the best
