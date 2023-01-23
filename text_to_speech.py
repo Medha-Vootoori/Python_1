15cbd459b52a096f53845e82ad61cc6ff4b6434e
Medha-Vootoori <121211005+Medha-Vootoori@users.noreply.github.com>
2023-01-23 23:11:26 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,12 +1,13 @@
 # import the pyttsx module inside program
 import pyttsx3
+print("Enter the text :")
 w=input()
 # initializing the module
-engine = pyttsx3.init()
+in_put = pyttsx3.init()
 
 # .say() function is used to speak the text you have written 
 # inside the function
-engine.say(w)
+in_put.say(w)
 
 # this is used to process and run the program commands
-engine.runAndWait()
+in_put.runAndWait()
